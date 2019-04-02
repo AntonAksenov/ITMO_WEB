cda9460b1c3501b6698e53c985527ab096d3761c
anton_axenov <anton.axenov.anton@gmail.com>
2019-04-02 21:47:41 +0300

    some minor changes

--- diploma/js/shop.js
+++ diploma/js/shop.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
     $("body").removeClass("preload");
-    $("#center-menu > ul > li")
 
     $(window).on("scroll resize", function () {
-        if ($(window).scrollTop() >= 240 && window.matchMedia('(max-width: 1180px)').matches) {
-            $('#center-menu').css({marginLeft: "60px", marginRight: "60px"});
+        if (window.matchMedia('(max-width: 1180px)').matches) {
+            if ($(window).scrollTop() >= 240) {
+                $('#center-menu').css({marginLeft: "60px", marginRight: "60px"});
+            } else {
+                $('#center-menu').css({marginLeft: "0", marginRight: "0"});
+            }
+            $("#left-menu-button, #right-menu-button").removeClass("active");
+            $("#left-menu").css({display: "none"});
         } else {
             $('#center-menu').css({marginLeft: "0", marginRight: "0"});
+            $("#left-menu-button, #right-menu-button").addClass("active");
         }
     });
 
@@ -15,6 +21,10 @@ $(document).ready(function () {
             $("#left-menu").css({display: "block"});
         } else {
             $("#left-menu").css({display: "none"});
+
+            if ($("#left-menu").css("display") == "block") {
+                $(this).toggleClass("active");
+            }
         }
     });
 
@@ -23,14 +33,43 @@ $(document).ready(function () {
             $("#right-menu").css({display: "block"});
         } else {
             $("#right-menu").css({display: "none"});
+            if ($("#right-menu").css("display") == "block") {
+                $(this).toggleClass("active");
+            }
         }
     });
 
-    $("#right-menu-button").click(function () {
-        $("#left-menu").toggleAttribute(hidden, false);
+    $("#center-menu > ul > li").click(function () {
+        $("#center-menu > ul > li").removeClass("active");
     });
 
     $("ul.nav > li").click(function () {
         $(this).toggleClass("active");
     });
+
+
+    function clearLeftMenu(curButton, colorClass) {
+        // removing all classes (preload is already removed, so we're removing only color)
+        $("body").removeClass().addClass(colorClass);
+        $("#left-menu li").removeClass("active");
+        // TODO: can we use "this" here instead of passing through function arguments????
+        $(curButton).addClass("active");
+        $("#name").text($(curButton).text());
+    }
+
+    $("#main-button").click(function () {
+        clearLeftMenu(this, "red");
+        // TODO: fill page with another content
+    });
+
+    $("#shop-button").click(function () {
+        clearLeftMenu(this, "blue");
+        // TODO: fill page with another content
+    });
+
+    $("#about-us-button").click(function () {
+        clearLeftMenu(this, "brown");
+        // TODO: fill page with another content
+    });
+
 });
